757d5e2d109fc39d5a1d94363545b602cac74f28
Lord-Y <Lord-Y@users.noreply.github.com>
2024-02-09 15:54:03 +0100

    Add update_golang_version.sh script

---
+++ update_golang_version.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+usage(){
+  echo "$1 argument is mantatory"
+  echo "Example: $0 1.21.6 1.22.0"
+}
+
+if [ -n "$1" ]
+then
+  export OLD_VERSION="$1"
+else
+  usage OLD_VERSION
+  exit 1
+fi
+shift
+
+if [ -n "$1" ]
+then
+  export NEW_VERSION="$1"
+else
+  usage NEW_VERSION
+  exit 1
+fi
+shift
+
+OS=$(uname -s | tr -s "A-Z" "a-z")
+if [[ "${OS}" == "darwin" ]]
+then
+  find . -type f -name go.mod -exec sed -i '' "s#^go ${OLD_VERSION}#go ${NEW_VERSION}#g" {} \;
+  sed -i '' "s#^go ${OLD_VERSION}#go ${NEW_VERSION}#g" .github/workflows/ci.yml
+else
+  find . -type f -name go.mod -exec sed -i "s#^go ${OLD_VERSION}#go ${NEW_VERSION}#g" {} \;
+  sed -i "s#^go ${OLD_VERSION}#go ${NEW_VERSION}#g" .github/workflows/ci.yml
+fi
+
+go mod tidy
