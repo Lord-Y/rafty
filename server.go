a47a8d2aa485ba6380f588766d2f32258e4d7fe6
Lord-Y <Lord-Y@users.noreply.github.com>
2024-05-30 16:24:10 +0200

    [rafty] - Update wording

--- server.go
+++ server.go
@@ -72,7 +72,7 @@ func (g *Server) Start() error {
 	g.Rafty.Address = g.Address
 	listener, err := net.Listen(g.Address.Network(), g.Address.String())
 	if err != nil {
-		return errors.Wrap(err, "Fail to listen gRPC server")
+		return errors.Wrap(err, "fail to listen gRPC server")
 	}
 	g.listener = listener
 
@@ -88,7 +88,7 @@ func (g *Server) Start() error {
 	grpcrequests.RegisterRaftyServer(g.server, pbSVC)
 	reflection.Register(g.server)
 
-	g.Rafty.Logger.Info().Msgf("Starting gRPC server at %s", g.Address.String())
+	g.Rafty.Logger.Info().Msgf("starting gRPC server at %s", g.Address.String())
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
@@ -127,5 +127,5 @@ func (g *Server) Stop() {
 	g.listener.Close()
 	g.server = nil
 	g.Rafty.disconnectToPeers()
-	g.Rafty.Logger.Info().Msg("Stopping gRPC server successful")
+	g.Rafty.Logger.Info().Msg("stopping gRPC server successful")
 }
