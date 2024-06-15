3102231a41ab0d44c5bbd768744360938f07a323
Lord-Y <Lord-Y@users.noreply.github.com>
2024-06-15 21:48:17 +0200

    [global] - Refactoring to better handle data races

---
+++ timers.go
@@ -0,0 +1,47 @@
+package rafty
+
+import (
+	"math/rand"
+	"time"
+)
+
+// randomElectionTimeout permit to generate a random value
+// that will be used during the election campain
+// when preVote set to true, a value will be generated for preVote election
+func (r *Rafty) randomElectionTimeout(preVote bool) time.Duration {
+	rd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	if preVote {
+		return time.Duration(preVoteElectionTimeoutMin+rd.Intn(preVoteElectionTimeoutMax-preVoteElectionTimeoutMin)) * time.Millisecond * time.Duration(r.TimeMultiplier)
+	}
+	return time.Duration(electionTimeoutMin+rd.Intn(electionTimeoutMax-electionTimeoutMin)) * time.Millisecond * time.Duration(r.TimeMultiplier)
+}
+
+// startElectionTimer permit during election campain to start electionTimer
+// when preVote set to true, preVoteElectionTimer will be restarted
+func (r *Rafty) startElectionTimer(preVote bool) {
+	if preVote {
+		r.preVoteElectionTimer = time.NewTimer(r.randomElectionTimeout(true))
+		return
+	}
+	r.electionTimer = time.NewTimer(r.randomElectionTimeout(false))
+}
+
+// resetElectionTimer permit during election campain to reset electionTimer
+// when preVote set to true, preVoteElectionTimer will be resetted
+func (r *Rafty) resetElectionTimer(preVote bool) {
+	if preVote {
+		r.preVoteElectionTimer.Reset(r.randomElectionTimeout(true))
+		return
+	}
+	r.electionTimer.Reset(r.randomElectionTimeout(false))
+}
+
+// stopElectionTimer permit during election campain to stop electionTimer
+// when preVote set to true, preVoteElectionTimer will be stopped
+func (r *Rafty) stopElectionTimer(preVote bool) {
+	if preVote {
+		r.preVoteElectionTimer.Stop()
+		return
+	}
+	r.electionTimer.Stop()
+}
