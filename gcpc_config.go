0d3691368c48e247d4ad2e91b780f017f2ad54cd
Lord-Y <Lord-Y@users.noreply.github.com>
2024-07-29 08:54:53 -0400

    [grpc] - Update server config

--- gcpc_config.go
+++ gcpc_config.go
@@ -12,15 +12,15 @@ var kaep = keepalive.EnforcementPolicy{
 }
 
 var kasp = keepalive.ServerParameters{
-	MaxConnectionIdle:     15 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
-	MaxConnectionAge:      30 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
-	MaxConnectionAgeGrace: 5 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
-	Time:                  5 * time.Second,  // Ping the client if it is idle for 5 seconds to ensure the connection is still active
-	Timeout:               1 * time.Second,  // Wait 1 second for the ping ack before assuming the connection is dead
+	MaxConnectionIdle:     150 * time.Second, // If a client is idle for 15 seconds, send a GOAWAY
+	MaxConnectionAge:      300 * time.Second, // If any connection is alive for more than 30 seconds, send a GOAWAY
+	MaxConnectionAgeGrace: 50 * time.Second,  // Allow 5 seconds for pending RPCs to complete before forcibly closing connections
+	Time:                  5 * time.Second,   // Ping the client if it is idle for 5 seconds to ensure the connection is still active
+	Timeout:               1 * time.Second,   // Wait 1 second for the ping ack before assuming the connection is dead
 }
 
-var kacp = keepalive.ClientParameters{
-	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
-	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
-	PermitWithoutStream: true,             // send pings even without active streams
-}
+// var kacp = keepalive.ClientParameters{
+// 	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+// 	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+// 	PermitWithoutStream: true,             // send pings even without active streams
+// }
