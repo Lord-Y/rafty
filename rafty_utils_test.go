7cd45183790e62e00a186e77b1a532790832f1ba
Lord-Y <Lord-Y@users.noreply.github.com>
2025-01-06 17:04:09 +0200

    test(rafty): Add more unit testing
    More TestStartXXX have been added in order to increase code coverage

--- rafty_utils_test.go
+++ rafty_utils_test.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 	"time"
 
@@ -17,13 +18,28 @@ import (
 )
 
 type clusterConfig struct {
-	t              *testing.T
-	clusterSize    uint
-	cluster        []*Rafty
-	TimeMultiplier uint
+	t                         *testing.T
+	runTestInParallel         bool
+	testName                  string
+	portStartRange            uint
+	clusterSize               uint
+	cluster                   []*Rafty
+	timeMultiplier            uint
+	delayLastNode             bool
+	delayLastNodeTimeDuration time.Duration
+	autoSetMinimumClusterSize bool
+	noDataDir                 bool
+	noNodeID                  bool
+	maxAppendEntries          uint64
 }
 
 func (cc *clusterConfig) makeCluster() (cluster []*Rafty) {
+	defaultPort := int(GRPCPort)
+	if cc.portStartRange == 0 {
+		cc.portStartRange = 50000
+	} else {
+		defaultPort = int(cc.portStartRange) + 51
+	}
 	for i := range cc.clusterSize {
 		var addr net.TCPAddr
 
@@ -31,7 +47,7 @@ func (cc *clusterConfig) makeCluster() (cluster []*Rafty) {
 		peers := []Peer{}
 		addr = net.TCPAddr{
 			IP:   net.ParseIP("127.0.0.5"),
-			Port: int(GRPCPort) + int(i),
+			Port: defaultPort + int(i),
 		}
 
 		for j := range cc.clusterSize {
@@ -39,14 +55,14 @@ func (cc *clusterConfig) makeCluster() (cluster []*Rafty) {
 				peerAddr string
 			)
 			if i == j {
-				peerAddr = fmt.Sprintf("127.0.0.5:500%d", 51+j)
+				peerAddr = fmt.Sprintf("127.0.0.5:%d", cc.portStartRange+51+j)
 			} else {
 				if i > 0 {
-					peerAddr = fmt.Sprintf("127.0.0.5:500%d", 51+j)
+					peerAddr = fmt.Sprintf("127.0.0.5:%d", cc.portStartRange+51+j)
 				} else if i > 1 {
-					peerAddr = fmt.Sprintf("127.0.0.5:500%d", 51+j+i+2)
+					peerAddr = fmt.Sprintf("127.0.0.5:%d", cc.portStartRange+51+j+i+2)
 				} else {
-					peerAddr = fmt.Sprintf("127.0.0.5:500%d", 51+j+i)
+					peerAddr = fmt.Sprintf("127.0.0.5:%d", cc.portStartRange+51+j+i)
 				}
 			}
 
@@ -56,9 +72,11 @@ func (cc *clusterConfig) makeCluster() (cluster []*Rafty) {
 				})
 
 				server = NewServer(addr)
-				server.ID = fmt.Sprintf("%d", i)
+				if !cc.noNodeID {
+					server.ID = fmt.Sprintf("%d", i)
+				}
 				server.Peers = peers
-				server.TimeMultiplier = 2
+				server.TimeMultiplier = cc.timeMultiplier
 			}
 		}
 		cluster = append(cluster, server)
@@ -69,8 +87,20 @@ func (cc *clusterConfig) makeCluster() (cluster []*Rafty) {
 func (cc *clusterConfig) startCluster() {
 	cc.cluster = cc.makeCluster()
 	for i, node := range cc.cluster {
-		cc.t.Run(fmt.Sprintf("cluster_%d", i), func(t *testing.T) {
-			node.DataDir = filepath.Join(os.TempDir(), "rafty", fmt.Sprintf("node%d", i))
+		if cc.autoSetMinimumClusterSize {
+			node.MinimumClusterSize = uint64(cc.clusterSize)
+		}
+		if cc.delayLastNode && i == 0 {
+			time.Sleep(cc.delayLastNodeTimeDuration)
+		}
+		node.MaxAppendEntries = cc.maxAppendEntries
+		cc.t.Run(fmt.Sprintf("cluster_%s_%d", cc.testName, i), func(t *testing.T) {
+			if cc.noDataDir && i != 0 {
+				node.DataDir = filepath.Join(os.TempDir(), "test_rafty", cc.testName, fmt.Sprintf("node%d", i))
+			}
+			if !cc.noDataDir {
+				node.DataDir = filepath.Join(os.TempDir(), "test_rafty", cc.testName, fmt.Sprintf("node%d", i))
+			}
 			// the following random sleep is necessary trick because when starting nth nodes during unit testing
 			// a race condition will be found in timers.go
 			// at r.preVoteElectionTimer = time.NewTimer(r.randomElectionTimeout(true))
@@ -144,4 +174,80 @@ func (cc *clusterConfig) clientGetLeader(nodeId int) (bool, string, string) {
 	}
 	cc.cluster[nodeId].Logger.Info().Msgf("Node %d reports that %s / %s is the leader", nodeId, response.GetLeaderAddress(), response.GetLeaderID())
 	return true, "", ""
+}
+
+func (cc *clusterConfig) testClustering(t *testing.T) {
+	// LogSource = cc.testName
+	if cc.runTestInParallel {
+		t.Parallel()
+	}
+	assert := assert.New(t)
+
+	submitCommandToNode := func(nodeId int) {
+		time.Sleep(20 * time.Second)
+		var found bool
+		for !found {
+			//nolint gosimple
+			select {
+			case <-time.After(30 * time.Millisecond):
+				found, _, _ = cc.clientGetLeader(nodeId)
+				if found {
+					for i, node := range cc.cluster {
+						cc.t.Run(fmt.Sprintf("%s_submitCommandToNode_%d_%d", cc.testName, nodeId, i), func(t *testing.T) {
+							_, err := node.SubmitCommand(command{kind: commandSet, key: fmt.Sprintf("key%d%d", nodeId, i), value: fmt.Sprintf("value%d", i)})
+							if err != nil {
+								node.Logger.Info().Msgf("LLLLLL %s", err.Error())
+								if strings.Contains(err.Error(), "the client connection is closing") {
+									assert.Contains(err.Error(), "the client connection is closing")
+								} else {
+									assert.Equal(fmt.Errorf("NoLeader"), err)
+								}
+							} else {
+								assert.Nil(err)
+							}
+						})
+					}
+				}
+			}
+		}
+	}
+
+	os.Setenv("RAFTY_LOG_LEVEL", "trace")
+	cc.startCluster()
+	time.Sleep(2 * time.Second)
+
+	startAndRestart := func(node int) {
+		cc.t.Run(fmt.Sprintf("startAndRestart_%d", node), func(t *testing.T) {
+			err := cc.startOrStopSpecificicNode(node, "start")
+			cc.cluster[node].Logger.Info().Msgf("MMMM %s", err.Error())
+			assert.Error(err)
+
+			time.Sleep(5 * time.Second)
+			err = cc.startOrStopSpecificicNode(node, "restart")
+			if err != nil {
+				cc.cluster[node].Logger.Info().Msgf("NNNN %s", err.Error())
+			}
+			assert.Nil(err)
+		})
+		time.Sleep(10 * time.Second)
+		submitCommandToNode(node)
+	}
+
+	time.Sleep(30 * time.Second)
+	node := 0
+	go startAndRestart(node)
+
+	time.Sleep(90 * time.Second)
+	node = 1
+	go startAndRestart(node)
+
+	time.Sleep(90 * time.Second)
+	node = 2
+	go startAndRestart(node)
+
+	time.Sleep(150 * time.Second)
+	os.Unsetenv("RAFTY_LOG_LEVEL")
+	cc.stopCluster()
+	err := os.RemoveAll(cc.cluster[node].DataDir)
+	assert.Nil(err)
 }
