1f01403732655ec1777539f955fd64b1d67dba57
Lord-Y <Lord-Y@users.noreply.github.com>
2025-07-24 18:03:44 +0200

    test(rafty): Add more tests for single server cluster

--- log_replication_test.go
+++ log_replication_test.go
@@ -1,8 +1,12 @@
 package rafty
 
 import (
+	"context"
+	"os"
+	"path/filepath"
 	"sync/atomic"
 	"testing"
+	"time"
 
 	"github.com/Lord-Y/rafty/raftypb"
 	"github.com/google/uuid"
@@ -88,4 +92,139 @@ func TestLogReplication_SendCatchupAppendEntries(t *testing.T) {
 		followerRepl.sendCatchupAppendEntries(client, oldRequest, oldResponse)
 	})
 	s.wg.Wait()
+}
+
+func TestLogReplication_singleServerCluster(t *testing.T) {
+	assert := assert.New(t)
+
+	t.Run("single_server_append_entries_quit_context", func(t *testing.T) {
+		s := singleServerClusterSetup()
+		s.isRunning.Store(true)
+		s.State = Leader
+		s.options.IsSingleServerCluster = true
+		s.options.PersistDataOnDisk = true
+		s.options.DataDir = filepath.Join(os.TempDir(), "rafty_singleServerAppendEntries")
+		state := leader{rafty: s}
+		currentTerm := s.currentTerm.Add(1)
+		s.quitCtx, s.stopCtx = context.WithCancel(context.Background())
+		s.stopCtx()
+		time.Sleep(100 * time.Millisecond) // make sure test always fail
+
+		entries := []*raftypb.LogEntry{
+			{
+				LogType:   uint32(logNoop),
+				Timestamp: uint32(time.Now().Unix()),
+				Term:      currentTerm,
+				Command:   nil,
+			},
+		}
+
+		totalLogs := s.logs.appendEntries(entries, false)
+		request := &onAppendEntriesRequest{
+			term:                       currentTerm,
+			prevLogIndex:               s.lastLogIndex.Load(),
+			prevLogTerm:                s.lastLogTerm.Load(),
+			totalLogs:                  uint64(totalLogs),
+			uuid:                       uuid.NewString(),
+			commitIndex:                s.commitIndex.Load(),
+			entries:                    entries,
+			membershipChangeInProgress: &atomic.Bool{},
+			replyToClient:              true,
+			replyToClientChan:          make(chan appendEntriesResponse, 1),
+		}
+
+		state.singleServerAppendEntries(request)
+		select {
+		case <-request.replyToClientChan:
+			t.Error("Should not receive any response")
+		default:
+			// expected
+		}
+	})
+
+	t.Run("single_server_append_timeout", func(t *testing.T) {
+		s := singleServerClusterSetup()
+		s.isRunning.Store(true)
+		s.State = Leader
+		s.options.PersistDataOnDisk = true
+		s.options.DataDir = filepath.Join(os.TempDir(), "rafty_singleServerAppendEntries")
+		state := leader{rafty: s}
+		currentTerm := s.currentTerm.Add(1)
+		s.quitCtx, s.stopCtx = context.WithCancel(context.Background())
+
+		entries := []*raftypb.LogEntry{
+			{
+				LogType:   uint32(logNoop),
+				Timestamp: uint32(time.Now().Unix()),
+				Term:      currentTerm,
+				Command:   nil,
+			},
+		}
+
+		totalLogs := s.logs.appendEntries(entries, false)
+		request := &onAppendEntriesRequest{
+			term:                       currentTerm,
+			prevLogIndex:               s.lastLogIndex.Load(),
+			prevLogTerm:                s.lastLogTerm.Load(),
+			totalLogs:                  uint64(totalLogs),
+			uuid:                       uuid.NewString(),
+			commitIndex:                s.commitIndex.Load(),
+			entries:                    entries,
+			membershipChangeInProgress: &atomic.Bool{},
+			replyToClient:              true,
+			replyToClientChan:          make(chan appendEntriesResponse), // must be unbuffered
+		}
+
+		start := time.Now()
+		state.singleServerAppendEntries(request)
+		elapsed := time.Since(start)
+
+		if elapsed < 500*time.Millisecond {
+			t.Errorf("expected at least 500ms got %v", elapsed)
+		}
+
+		select {
+		case <-request.replyToClientChan:
+			t.Error("Should not receive any response")
+		default:
+			// expected
+		}
+	})
+
+	t.Run("reply_to_chan", func(t *testing.T) {
+		s := singleServerClusterSetup()
+		s.isRunning.Store(true)
+		s.State = Leader
+		s.options.PersistDataOnDisk = true
+		s.options.DataDir = filepath.Join(os.TempDir(), "rafty_singleServerAppendEntries")
+		state := leader{rafty: s}
+		currentTerm := s.currentTerm.Add(1)
+
+		entries := []*raftypb.LogEntry{
+			{
+				LogType:   uint32(logNoop),
+				Timestamp: uint32(time.Now().Unix()),
+				Term:      currentTerm,
+				Command:   nil,
+			},
+		}
+
+		totalLogs := s.logs.appendEntries(entries, false)
+		request := &onAppendEntriesRequest{
+			term:                       currentTerm,
+			prevLogIndex:               s.lastLogIndex.Load(),
+			prevLogTerm:                s.lastLogTerm.Load(),
+			totalLogs:                  uint64(totalLogs),
+			uuid:                       uuid.NewString(),
+			commitIndex:                s.commitIndex.Load(),
+			entries:                    entries,
+			membershipChangeInProgress: &atomic.Bool{},
+			replyToClient:              true,
+			replyToClientChan:          make(chan appendEntriesResponse, 1),
+		}
+
+		state.singleServerAppendEntries(request)
+		data := <-request.replyToClientChan
+		assert.Nil(data.Error)
+	})
 }
