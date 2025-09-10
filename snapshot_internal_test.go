0ef77d42a78e69c9741d7d25618cf4a2251ff996
Lord-Y <Lord-Y@users.noreply.github.com>
2025-09-10 19:58:02 +0200

    chore(snapshot): Move snapshot mocks into snapshot mock file

--- snapshot_internal_test.go
+++ snapshot_internal_test.go
@@ -1,10 +1,8 @@
 package rafty
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
-	"io"
 	"os"
 	"testing"
 
@@ -12,63 +10,6 @@ import (
 	"github.com/jackc/fake"
 	"github.com/stretchr/testify/assert"
 )
-
-type mockSnapshot struct {
-	discardCalled int
-}
-
-type mockSnapshotter struct {
-	prepareErr error
-	snap       *mockSnapshot
-}
-
-func (m *mockSnapshot) Name() string { return "mock" }
-func (m *mockSnapshot) Discard() error {
-	m.discardCalled++
-	return errors.New("discard error")
-}
-func (m *mockSnapshot) Close() error { return errors.New("close error") }
-func (m *mockSnapshot) Metadata() SnapshotMetadata {
-	return SnapshotMetadata{}
-}
-func (m *mockSnapshot) Read(p []byte) (n int, err error) {
-	return 0, errors.New("read error")
-}
-func (m *mockSnapshot) Reader() (bytes.Buffer, error) {
-	var x bytes.Buffer
-	return x, errors.New("buffer error")
-}
-
-func (m *mockSnapshot) Seek(offset int64, whence int) (int64, error) {
-	return 0, errors.New("write error")
-}
-func (m *mockSnapshot) Write(p []byte) (n int, err error) {
-	return 0, errors.New("seek error")
-}
-
-func (m *mockSnapshotter) List() (l []*SnapshotMetadata) {
-	return
-}
-
-func (m *mockSnapshotter) PrepareSnapshotWriter(lastIncludedIndex, lastIncludedTerm, lastAppliedConfigIndex, lastAppliedConfigTerm uint64, currentConfig Configuration) (Snapshot, error) {
-	return m.snap, m.prepareErr
-}
-
-func (m *mockSnapshotter) PrepareSnapshotReader(name string) (Snapshot, io.ReadCloser, error) {
-	return nil, nil, errors.New("fail to prepare snapshot reader")
-}
-
-type mockFSM struct {
-	snapshotErr error
-}
-
-func (m *mockFSM) Snapshot(w io.Writer) error {
-	return m.snapshotErr
-}
-
-func (m *mockFSM) Restore(io.Reader) error {
-	return m.snapshotErr
-}
 
 func TestSnapshot_internal(t *testing.T) {
 	assert := assert.New(t)
