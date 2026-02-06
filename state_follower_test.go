861f5b051291e2ac81b33536d429590aa47b5b55
Lord-Y <Lord-Y@users.noreply.github.com>
2026-02-06 14:00:55 +0100

    fix(rafty): Fix blocking start of the cluster

--- state_follower_test.go
+++ state_follower_test.go
@@ -34,6 +34,32 @@ func TestStateFollower(t *testing.T) {
 		state.onTimeout()
 	})
 
+	t.Run("onTimeout_minimum_cluster_size_not_reached", func(t *testing.T) {
+		s := basicNodeSetup()
+		defer func() {
+			assert.Nil(s.logStore.Close())
+			assert.Nil(os.RemoveAll(getRootDir(s.options.DataDir)))
+		}()
+		s.isRunning.Store(true)
+		s.State = Follower
+		state := follower{rafty: s}
+		t.Run("init", func(t *testing.T) {
+			state.init()
+		})
+
+		s.State = Follower
+		s.minimumClusterSizeReach.Store(false)
+		s.askForMembershipInProgress.Store(false)
+		s.askForMembership.Store(false)
+		s.decommissioning.Store(false)
+		s.setLeader(leaderMap{})
+
+		state.onTimeout()
+
+		assert.Equal(Follower, s.getState())
+		assert.False(s.minimumClusterSizeReach.Load())
+	})
+
 	t.Run("release", func(t *testing.T) {
 		s.State = Follower
 		state.release()
